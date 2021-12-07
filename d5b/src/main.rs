8db1a6a221fffec78647cf45475e907a9e38093e
Brian Lee <briandl92391@gmail.com>
2021-12-06 16:15:58 -0800

    Day 5 done

--- d5b/src/main.rs
+++ d5b/src/main.rs
@@ -1,112 +1,15 @@
 use std::str::FromStr;
-use std::num::ParseIntError;
 use std::ops::Deref;
-use y2021::ocean_vents::{OceanVentMap};
+use y2021::ocean_vents::{OceanVentMap, Coordinate, Ray, RayParseError};
 use y2021::utils;
-
-#[derive(PartialEq, Debug)]
-enum CoordinateParseError {
-    InvalidFormat(ParseIntError),
-    IncorrectValueCount(usize),
-}
-
-#[derive(Clone, Debug, PartialEq)]
-struct Coordinate {
-    x: usize,
-    y: usize,
-}
-
-impl FromStr for Coordinate {
-    type Err = CoordinateParseError;
-
-    fn from_str(s: &str) -> Result<Self, Self::Err> {
-        match s.split(",").collect::<Vec<&str>>() {
-            x if x.len() != 2 => Err(CoordinateParseError::IncorrectValueCount(x.len())),
-            x => Ok(Coordinate{
-                x: x[0].parse::<usize>().map_err(|e| CoordinateParseError::InvalidFormat(e))?,
-                y: x[1].parse::<usize>().map_err(|e| CoordinateParseError::InvalidFormat(e))?,
-            }),
-        }
-    }
-}
-
-#[derive(PartialEq, Debug)]
-enum RayParseError {
-    InvalidFormat,
-    InvalidCoordinate(CoordinateParseError),
-    IncorrectCoordinateCount(usize),
-}
-
-#[derive(Clone, Debug)]
-struct Ray {
-    src: Coordinate,
-    dst: Coordinate,
-}
-
-impl Ray {
-    pub fn is_horizontal(&self) -> bool {
-        self.src.y == self.dst.y
-    }
-
-    pub fn is_vertical(&self) -> bool {
-        self.src.x == self.dst.x
-    }
-
-    pub fn points(&self) -> Vec<Coordinate> {
-        let mut result: Vec<Coordinate> = Vec::new();
-        let mut position = self.src.clone();
-
-        // println!("Finding points from {:?} to {:?}", self.src, self.dst);
-
-        loop {
-            // println!(" - [{}] {},{}", result.len(), position.x, position.y);
-
-            result.push(position.clone());
-
-            if position == self.dst {
-                break;
-            }
-
-            if position.x == self.dst.x {
-                if position.y < self.dst.y {
-                    position.y += 1;
-                } else {
-                    position.y -= 1;
-                }
-            } else {
-                if position.x < self.dst.x {
-                    position.x += 1;
-                } else {
-                    position.x -= 1;
-                }
-            }
-        }
-
-        return result;
-    }
-}
-
-impl FromStr for Ray {
-    type Err = RayParseError;
-
-    fn from_str(s: &str) -> Result<Self, Self::Err> {
-        match s.split(" -> ").collect::<Vec<&str>>() {
-            x if x.len() != 2 => Err(RayParseError::IncorrectCoordinateCount(x.len())),
-            x => Ok(Ray{
-                src: Coordinate::from_str(x[0]).map_err(|e| RayParseError::InvalidCoordinate(e))?,
-                dst: Coordinate::from_str(x[1]).map_err(|e| RayParseError::InvalidCoordinate(e))?,
-            }),
-        }
-    }
-}
 
 fn parse(lines: Vec<String>) -> Result<Vec<Ray>, RayParseError> {
     lines.into_iter().map(|line| Ray::from_str(line.as_str())).collect()
 }
 
 fn main() {
-    println!("Starting Day 5a");
-    println!("Finding vents.");
+    println!("Starting Day 5b");
+    println!("Finding vents (allowing diagonals).");
 
     let rays = parse(
         utils::read_input("./input/input.txt").lines().map(|l| l.to_string()).collect::<Vec<String>>()
@@ -115,10 +18,6 @@ fn main() {
     let mut vent_map = OceanVentMap::new();
 
     for ray in rays {
-        if !ray.is_horizontal() && !ray.is_vertical() {
-            continue;
-        }
-
         for coordinate in ray.points() {
             vent_map.increment(coordinate.x, coordinate.y);
         }
--- y2021/src/ocean_vents.rs
+++ y2021/src/ocean_vents.rs
@@ -1,5 +1,105 @@
 use std::collections::hash_map::HashMap;
+use std::num::ParseIntError;
 use std::ops::AddAssign;
+use std::str::FromStr;
+
+#[derive(PartialEq, Debug)]
+pub enum CoordinateParseError {
+    InvalidFormat(ParseIntError),
+    IncorrectValueCount(usize),
+}
+
+#[derive(Clone, Debug, PartialEq)]
+pub struct Coordinate {
+    pub x: usize,
+    pub y: usize,
+}
+
+impl FromStr for Coordinate {
+    type Err = CoordinateParseError;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        match s.split(",").collect::<Vec<&str>>() {
+            x if x.len() != 2 => Err(CoordinateParseError::IncorrectValueCount(x.len())),
+            x => Ok(Coordinate{
+                x: x[0].parse::<usize>().map_err(|e| CoordinateParseError::InvalidFormat(e))?,
+                y: x[1].parse::<usize>().map_err(|e| CoordinateParseError::InvalidFormat(e))?,
+            }),
+        }
+    }
+}
+
+#[derive(PartialEq, Debug)]
+pub enum RayParseError {
+    InvalidFormat,
+    InvalidCoordinate(CoordinateParseError),
+    IncorrectCoordinateCount(usize),
+}
+
+#[derive(Clone, Debug)]
+pub struct Ray {
+    pub src: Coordinate,
+    pub dst: Coordinate,
+}
+
+impl Ray {
+    pub fn is_horizontal(&self) -> bool {
+        self.src.y == self.dst.y
+    }
+
+    pub fn is_vertical(&self) -> bool {
+        self.src.x == self.dst.x
+    }
+
+    pub fn points(&self) -> Vec<Coordinate> {
+        let mut result: Vec<Coordinate> = Vec::new();
+        let mut position = self.src.clone();
+
+        // println!("Finding points from {:?} to {:?}", self.src, self.dst);
+
+        loop {
+            // println!(" - [{}] {},{}", result.len(), position.x, position.y);
+
+            result.push(position.clone());
+
+            if position == self.dst {
+                break;
+            }
+
+            if position.y != self.dst.y {
+                if position.y < self.dst.y {
+                    position.y += 1;
+                } else {
+                    position.y -= 1;
+                }
+            }
+
+            if position.x != self.dst.x {
+                if position.x < self.dst.x {
+                    position.x += 1;
+                } else {
+                    position.x -= 1;
+                }
+            }
+        }
+
+        return result;
+    }
+}
+
+impl FromStr for Ray {
+    type Err = RayParseError;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        match s.split(" -> ").collect::<Vec<&str>>() {
+            x if x.len() != 2 => Err(RayParseError::IncorrectCoordinateCount(x.len())),
+            x => Ok(Ray{
+                src: Coordinate::from_str(x[0]).map_err(|e| RayParseError::InvalidCoordinate(e))?,
+                dst: Coordinate::from_str(x[1]).map_err(|e| RayParseError::InvalidCoordinate(e))?,
+            }),
+        }
+    }
+}
 
 pub struct OceanVentMap {
     pub rows: usize,
