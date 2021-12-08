e39508d32b79198262b200678152114733f41223
Brian Lee <briandl92391@gmail.com>
2021-12-07 17:57:46 -0800

    Day 6b

---
+++ d6b/src/main.rs
@@ -0,0 +1,20 @@
+use std::str::FromStr;
+use y2021::lanternfish::{SpawningModel};
+use y2021::utils;
+
+fn main() {
+    println!("Starting Day 6a");
+    println!("Modeling lanternfish growth.");
+
+    let numbers = utils::read_input("./input/input.txt").trim()
+        .split(",").map(|l| l.parse::<u64>().unwrap()).collect::<Vec<u64>>();
+    let days_to_simulate = 256;
+
+    let mut model = SpawningModel::new(numbers);
+
+    for i in 0..days_to_simulate {
+        model.one_day();
+    }
+
+    println!("Number of fish after {} days: {}", days_to_simulate, model.get_total());
+}
--- y2021/src/lanternfish.rs
+++ y2021/src/lanternfish.rs
@@ -2,57 +2,54 @@ use std::collections::hash_map::HashMap;
 use std::collections::hash_set::HashSet;
 use std::ops::{Add, AddAssign};
 
-const SPAWN_INTERVAL: u32 = 7;
-const OFFSPRING_DELAY: u32 = 2;
+const SPAWN_INTERVAL: u64 = 7;
+const OFFSPRING_DELAY: u64 = 2;
 
 pub struct SpawningModel {
-    pub members: HashMap<u32, HashSet<u32>>,
+    pub members: HashMap<u64, u64>,
     pub days: usize,
-    next_id: u32,
 }
 
 impl SpawningModel {
-    pub fn new(initial: Vec<u32>) -> SpawningModel {
-        let mut model = SpawningModel { members: HashMap::new(), days: 0, next_id: 0 };
+    pub fn new(initial: Vec<u64>) -> SpawningModel {
+        let mut model = SpawningModel { members: HashMap::new(), days: 0 };
 
         for i in 0..(SPAWN_INTERVAL + OFFSPRING_DELAY) {
-            model.members.insert(i as u32, HashSet::new());
+            model.members.insert(i as u64, 0);
         }
 
         initial.into_iter().for_each(|f| {
-            model.members.get_mut(&f).unwrap().insert(model.next_id);
-            model.next_id.add_assign(1);
+            model.members.get_mut(&f).map(|x| x.add_assign(1));
         });
 
         model
     }
 
     pub fn get_total(&self) -> usize {
-        return self.next_id as usize;
+        self.members.values().fold(0_u64, |result, m| result + *m) as usize
+    }
+
+    pub fn print_counts(&self) {
+        println!("Printing fish counts");
+        for i in 0..(SPAWN_INTERVAL + OFFSPRING_DELAY) {
+            println!(" - {} DAY(S) REMAINING = {}", i, self.members[&i]);
+        }
     }
 
     pub fn one_day(&mut self) {
-        println!("Simulating Day {}", self.days + 1);
+        let mut members: HashMap<u64, u64> = HashMap::new();
 
-        let mut members: HashMap<u32, HashSet<u32>> = HashMap::new();
-
-        for i in 0..(SPAWN_INTERVAL + OFFSPRING_DELAY) {
-            members.insert(i as u32, HashSet::new());
-        }
-
-        (0..(SPAWN_INTERVAL + OFFSPRING_DELAY)).into_iter().flat_map(|i| {
-            self.members[&(i as u32)].clone().into_iter().map(|id| (i, id)).collect::<Vec<(u32, u32)>>()
-        }).for_each(|(days_remaining, id)| {
-            if days_remaining == 0 {
-                println!(" - Fish {} spawned offspring {}. Fish {} at {}; Fish {} at {}.", id, self.next_id, id, SPAWN_INTERVAL - 1, self.next_id, SPAWN_INTERVAL + OFFSPRING_DELAY - 1);
-                members.get_mut(&(SPAWN_INTERVAL + OFFSPRING_DELAY - 1)).unwrap().insert(self.next_id);
-                members.get_mut(&(SPAWN_INTERVAL - 1)).unwrap().insert(id);
-                self.next_id.add_assign(1);
-            } else {
-                println!(" - Fish {} at {}.", id, days_remaining - 1);
-                members.get_mut(&(days_remaining - 1)).unwrap().insert(id);
-            }
+        (1..(SPAWN_INTERVAL + OFFSPRING_DELAY)).for_each(|i| {
+            members.insert(i - 1, self.members[&i]);
         });
+
+        let reproduced_count = self.members[&0];
+
+        members.insert(
+            SPAWN_INTERVAL - 1,
+            members.get(&(SPAWN_INTERVAL - 1)).map(|x| *x + reproduced_count).unwrap_or(0)
+        );
+        members.insert(SPAWN_INTERVAL + OFFSPRING_DELAY - 1, reproduced_count);
 
         self.members = members;
         self.days.add_assign(1);
